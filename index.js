6390224553315970da0e2977f4053e940dafaa61
Jatin Raghav <Jatin.Raghav@rtctek.global>
2024-12-09 18:13:56 +0530

    Your commit message describing changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
-app.use(express.json());
 
 const items = require('./router/route');
+const count = require('./router/items');
 
 app.use('/api',items);
+app.use('/item',count);
 
 app.listen(port, () => {
   console.log(`VILLEN listening on port ${port}`)
